02137c469ac27928c53e07d160c4e8c1a949530e
Alex Erygin <alex.erygin@gmail.com>
2015-10-05 23:51:43 +0300

    get and set methods implemented

--- sources/SafeConfig/Scanner.cs
+++ sources/SafeConfig/Scanner.cs
@@ -1,11 +1,15 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Nelibur.Sword.DataStructures;
+using Nelibur.Sword.Extensions;
 
 namespace SafeConfig
 {
     public class Scanner
     {
+	    private readonly Dictionary<string, object> storedValues = new Dictionary<string, object>(); 
+
 	    public Option<Scanner> AtFolder(string folder)
 	    {
 		    if (!Directory.Exists(folder))
@@ -20,6 +24,22 @@ namespace SafeConfig
 		    }
 
 		    return Option<Scanner>.Empty;
+	    }
+
+	    public Option<Scanner> Load()
+	    {
+		    return this.ToOption();
+	    }
+
+	    public Option<Scanner> Set<T>(string key, T value)
+	    {
+		    storedValues[key] = value;
+			return this.ToOption();
+		}
+
+	    public Option<T> Get<T>(string key)
+	    {
+		    return !storedValues.ContainsKey(key) ? Option<T>.Empty : ((T) storedValues[key]).ToOption();
 	    }
     }
 }
--- sources/UnitTests/ScannerTests.cs
+++ sources/UnitTests/ScannerTests.cs
@@ -1,4 +1,6 @@
-﻿using Nelibur.Sword.DataStructures;
+﻿using System;
+using Nelibur.Sword.DataStructures;
+using Nelibur.Sword.Extensions;
 using SafeConfig;
 using Xunit;
 
@@ -7,14 +9,15 @@ namespace UnitTests
 	public class ScannerTests
 	{
 		[Fact]
-		void Scan_ReturnsOption()
+		void AtFolder_ReturnsOption()
 		{
 			var scanner = new Scanner();
-			Assert.IsType<Option<Scanner>>(scanner.AtFolder("2ED1FA2A-62B3-46E4-BB02-24008FA4373A"));
+			Assert.IsType<Option<Scanner>>(
+				scanner.AtFolder("2ED1FA2A-62B3-46E4-BB02-24008FA4373A"));
 		}
 
 		[Fact]
-		void Scan_NotExistingFolder_EmptyOption()
+		void AtFolder_NotExistingFolder_EmptyOption()
 		{
 			var scanner = new Scanner();
 			var result = scanner.AtFolder("31032EB7-F965-4FEC-9E51-D58F896AE482");
@@ -22,7 +25,7 @@ namespace UnitTests
 		}
 
 		[Fact]
-		void Scan_ExistingDirectoryWithoutRequiredContent_EmptyOption()
+		void AtFolder_ExistingDirectoryWithoutRequiredContent_EmptyOption()
 		{
 			var scanner = new Scanner();
 			var result = scanner.AtFolder(@"data\empty\");
@@ -30,11 +33,26 @@ namespace UnitTests
 		}
 
 		[Fact]
-		void Scan_ExistingDirectoryWithRequiredContent_NotEmpty()
+		void AtFolder_ExistingDirectoryWithRequiredContent_NotEmpty()
 		{
 			var scanner = new Scanner();
-			var result = scanner.AtFolder(@"data\with-settings\");
+			var result = scanner.AtFolder(@"data\with-settings\")
+				.Do(x => x.Load());
 			Assert.NotEqual(Option<Scanner>.Empty, result);
+		}
+
+		[Theory]
+		[InlineData("sql-connection", "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;")]
+		[InlineData("password","qwerty")]
+		[InlineData("id", "B9D46109-B6F7-45FD-8DE4-98E8975D6E6F")]
+		void Set_SomeKeysAndValues_GetAndEqual(string key, object value)
+		{
+			string result = null;
+			var scanner = new Scanner()
+				.Set(key, value)
+				.Do(x=>result = x.Get<string>(key).Value);
+
+			Assert.Equal(result, value);
 		}
 	}
 }
