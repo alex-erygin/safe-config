064cc7ba26268b3d09da73a62838e498bf080b36
Alex Erygin <alex.erygin@gmail.com>
2015-10-05 22:43:05 +0300

    just started

---
+++ sources/SafeConfig/Scanner.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using Nelibur.Sword.DataStructures;
+
+namespace SafeConfig
+{
+    public class Scanner
+    {
+	    public Option<Scanner> AtFolder(string folder)
+	    {
+		    if (!Directory.Exists(folder))
+		    {
+			    return Option<Scanner>.Empty;
+		    }
+
+		    var files = Directory.GetFiles(folder, "*.safeconfig");
+		    if (files.Any())
+		    {
+			    return new Option<Scanner>(this);
+		    }
+
+		    return Option<Scanner>.Empty;
+	    }
+    }
+}
---
+++ sources/UnitTests/ScannerTests.cs
@@ -0,0 +1,40 @@
+﻿using Nelibur.Sword.DataStructures;
+using SafeConfig;
+using Xunit;
+
+namespace UnitTests
+{
+	public class ScannerTests
+	{
+		[Fact]
+		void Scan_ReturnsOption()
+		{
+			var scanner = new Scanner();
+			Assert.IsType<Option<Scanner>>(scanner.AtFolder("2ED1FA2A-62B3-46E4-BB02-24008FA4373A"));
+		}
+
+		[Fact]
+		void Scan_NotExistingFolder_EmptyOption()
+		{
+			var scanner = new Scanner();
+			var result = scanner.AtFolder("31032EB7-F965-4FEC-9E51-D58F896AE482");
+			Assert.Equal(Option<Scanner>.Empty, result);
+		}
+
+		[Fact]
+		void Scan_ExistingDirectoryWithoutRequiredContent_EmptyOption()
+		{
+			var scanner = new Scanner();
+			var result = scanner.AtFolder(@"data\empty\");
+			Assert.Equal(Option<Scanner>.Empty, result);
+		}
+
+		[Fact]
+		void Scan_ExistingDirectoryWithRequiredContent_NotEmpty()
+		{
+			var scanner = new Scanner();
+			var result = scanner.AtFolder(@"data\with-settings\");
+			Assert.NotEqual(Option<Scanner>.Empty, result);
+		}
+	}
+}
