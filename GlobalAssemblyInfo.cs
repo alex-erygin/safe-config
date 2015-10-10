adcf603e76ec33c6ad517e2c65259e706d157fe3
Alex Erygin <alex.erygin@gmail.com>
2015-10-10 09:42:55 +0300

    use Global assembly info

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿[assembly: System.Reflection.AssemblyProduct("Safe config")]
+[assembly: System.Reflection.AssemblyCompany("Aleksandr Erygin")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright 2015 Aleksandr Erygin.")]
+[assembly: System.Reflection.AssemblyTrademark("Safe config")]
+[assembly: System.Reflection.AssemblyConfiguration("Release")]
+[assembly: System.Reflection.AssemblyVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("Compiled By alex")]
+
+
